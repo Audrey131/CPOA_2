ae940361101cf5ffa95fa31a7417445d262d47f9
Audrey <audreyg0301@gmail.com>
2021-09-13 15:20:50 +0200

    Projet CPOA

---
+++ Documents/DUT 2eme année/javaencore/td1v2/src/Connexion.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class Connexion {
+
+	public Connection creeConnexion() {
+
+		String url ="jdbc:mysql://devbdd.iutmetz.univ-lorraine.fr:3306/girard144u_RevuesOnLine";
+		String login = "girard144u_appli";
+		String pwd = "8756931";
+		Connection maConnexion = null;
+
+		try {
+			maConnexion = DriverManager.getConnection(url, login, pwd);
+			System.out.println("Connexion établie");
+		} catch (SQLException sqle) {
+			System.out.println("Erreur connexion" + sqle.getMessage());
+		}
+        
+		return maConnexion;
+	}	
+}
---
+++ Documents/DUT 2eme année/javaencore/td1v2/src/lancer.java
@@ -0,0 +1,15 @@
+public class lancer {
+	
+	//private static Connexion Connection;
+	public static void main(String[] args) {
+		System.out.println("def");
+		Periodicite p = new Periodicite();
+		//Abonnement ab = new Abonnement();
+		
+		p.AddPerio("connard");
+		//p.SuppPerio(10);
+		//p.ModPerio(7, "bidule");
+
+		//ab.AddAbo("2002-04-01", "2002-04-02", 1, 1);
+	}
+}
